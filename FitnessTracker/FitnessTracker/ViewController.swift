0f90161175fe2e9694aac8b5b7bea27a56239b75
ramanjaneyulukoduri <ramanjaneyulu.koduri@griffithuni.edu.au>
2022-06-18 09:20:00 +0530

    Project Setup

---
+++ FitnessTracker/FitnessTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessTracker
+//
+//  Created by Ajay Girolkar on 18/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

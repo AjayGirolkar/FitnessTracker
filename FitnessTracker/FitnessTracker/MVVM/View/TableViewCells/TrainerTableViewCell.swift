0f593e3fc611e6999588122aa2bee8a2037b7ea0
AjayGirolkar <ajaygirolkar@gmail.com>
2022-07-07 11:05:22 +0530

    Added empty TrainerTableViewCell

---
+++ FitnessTracker/FitnessTracker/MVVM/View/TableViewCells/TrainerTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TrainerTableViewCell.swift
+//  FitnessTracker
+//
+//  Created by Ajay Girolkar on 07/07/22.
+//
+
+import UIKit
+
+class TrainerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

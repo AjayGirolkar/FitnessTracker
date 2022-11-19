0b3de69413d958b7a868e7eddf8bd8f016086389
AjayGirolkar <ajaygirolkar@gmail.com>
2022-11-19 10:20:27 +0530

    disable default call

--- FitnessTracker/FitnessTracker/MVVM/View/ViewController/LoginViewController.swift
+++ FitnessTracker/FitnessTracker/MVVM/View/ViewController/LoginViewController.swift
@@ -17,7 +17,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        SharedManager.setupDefaultData()
+        //SharedManager.setupDefaultData()
     }
     
     
